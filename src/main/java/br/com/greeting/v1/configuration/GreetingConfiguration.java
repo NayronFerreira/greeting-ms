5f037b2456d3e1bd5fe20cec595d71a2e0e5b0b5
nayron.ferreira <May@211021>
2021-12-22 20:38:08 -0300

    Implementando Actuactor no YML do greeting  e implementando RefreshScope

--- src/main/java/br/com/greeting/v1/configuration/GreetingConfiguration.java
+++ src/main/java/br/com/greeting/v1/configuration/GreetingConfiguration.java
@@ -3,8 +3,10 @@ package br.com.greeting.v1.configuration;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
+@RefreshScope
 @Component
 @ConfigurationProperties("greeting-service")
 @Data
